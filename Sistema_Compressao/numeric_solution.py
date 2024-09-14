ccdaa533da01c9443cf61c44e3d6697cbee0c6e4
MMatheusB <lastb0402@gmail.com>
2024-09-14 13:16:08 -0300

    fixing some details

--- Sistema_Compressao/numeric_solution.py
+++ Sistema_Compressao/numeric_solution.py
@@ -2,29 +2,23 @@ import casadi as ca
 import numpy as np
 import matplotlib.pyplot as plt
 
-A1 = 2.6e-3
+A1 = (2.6)*(10**-3)
 Lc = 2
 kv = 0.38
 P1 = 8.5
 P_out = 5
 C = 479
-initial_conditions = [7.948, 27/4]
-intervalo = [np.linspace(0,60,100), np.linspace(60,120,100),np.linspace(120,180,100),np.linspace(180,240,100),np.linspace(240,300,100)]
+a = 0.5289
+b = 51/4
+np.random.seed(42)
+intervalo = [np.linspace(0,60,1000000), np.linspace(60,120,1000000),np.linspace(120,180,1000000),np.linspace(180,240,1000000),np.linspace(240,300,1000000)]
 x = ca.MX.sym('x', 2)
 alpha = ca.MX.sym('alpha', 1)
 x0_values = []
 x1_values = []
 
-for i in range(0,5):
-    rhs = ca.vertcat((A1/Lc)*((1.5 * P1) - x[1]), (C**2)/2 * (x[0] - alpha * kv * np.sqrt(x[1] - P_out)))
 
-    ode = {}
-    ode['x'] = x
-    ode['ode'] = rhs
-    ode['p'] = alpha
-
-    F = ca.integrator('F','cvodes', ode, 0, intervalo[i])
-    
+for i in range(0,5):  
     if i ==0 or i ==2 or i==4:
         alpha0 = 0.5
     elif i == 1:
@@ -32,15 +26,22 @@ for i in range(0,5):
     elif i ==3:
         alpha0 = 0.54
     
-    sol = F(x0 = [27/4, 7.948], p = alpha0)
+    rhs = ca.vertcat((A1/Lc)*((1.5 * P1) - x[1]), (C**2)/2 * (x[0] - alpha * kv * np.sqrt(x[1] - P_out)))
+    ode = {'x' : x, 'ode' : rhs, 'p' : alpha }
+
+    F = ca.integrator('F','cvodes', ode, intervalo[i][0], intervalo[i])
+    
+    sol = F(x0 = [a, b], p = alpha0)
 
     xf_values = np.array(sol["xf"])
 
     aux1, aux2 = xf_values
-
+    print(xf_values)
     x0_values.append(aux1)
     x1_values.append(aux2)
-    initial_conditions = [x0_values[-1], x1_values[-1]]
+    a = aux1[-1]
+    b = aux2[-1]
+
 
 plt.figure()
 for i in range(0,5):
