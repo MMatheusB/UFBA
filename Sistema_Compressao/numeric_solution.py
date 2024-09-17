ca142aab4b164bcb916cf56de22d35563e55fe49
MMatheusB <lastb0402@gmail.com>
2024-09-17 14:13:33 -0300

    fsolve

--- Sistema_Compressao/numeric_solution.py
+++ Sistema_Compressao/numeric_solution.py
@@ -1,6 +1,13 @@
+from scipy.optimize import fsolve
 import casadi as ca
 import numpy as np
 import matplotlib.pyplot as plt
+
+def fun(variables, A1, Lc, kv, P1, P_out, C) :
+    (x,y) = variables
+    eqn_1 = (A1/Lc)* ((1.5 * P1) - y)
+    eqn_2 = (C**2)/2 * (x - 0.5 * kv * np.sqrt(y - P_out))
+    return [eqn_1, eqn_2]
 
 A1 = (2.6)*(10**-3)
 Lc = 2
@@ -8,10 +15,14 @@ kv = 0.38
 P1 = 8.5
 P_out = 5
 C = 479
-a = 0.5289
-b = 51/4
+
+result = fsolve(fun, (0, 10), args = (A1, Lc, kv, P1, P_out, C)) 
+print(result)
+
+a = result[0]
+b = result[1]
 np.random.seed(42)
-intervalo = [np.linspace(0,60,1000000), np.linspace(60,120,1000000),np.linspace(120,180,1000000),np.linspace(180,240,1000000),np.linspace(240,300,1000000)]
+intervalo = [np.linspace(0,400,1000), np.linspace(400,800,1000),np.linspace(800,1200,1000),np.linspace(1200,1600,1000),np.linspace(1600,2000,1000)]
 x = ca.MX.sym('x', 2)
 alpha = ca.MX.sym('alpha', 1)
 x0_values = []
@@ -19,12 +30,10 @@ x1_values = []
 
 
 for i in range(0,5):  
-    if i ==0 or i ==2 or i==4:
+    if i ==0:
         alpha0 = 0.5
-    elif i == 1:
-        alpha0 = 0.46
-    elif i ==3:
-        alpha0 = 0.54
+    else:
+        alpha0 = np.random.uniform(0.2, 0.8)
     
     rhs = ca.vertcat((A1/Lc)*((1.5 * P1) - x[1]), (C**2)/2 * (x[0] - alpha * kv * np.sqrt(x[1] - P_out)))
     ode = {'x' : x, 'ode' : rhs, 'p' : alpha }
@@ -36,7 +45,6 @@ for i in range(0,5):
     xf_values = np.array(sol["xf"])
 
     aux1, aux2 = xf_values
-    print(xf_values)
     x0_values.append(aux1)
     x1_values.append(aux2)
     a = aux1[-1]
