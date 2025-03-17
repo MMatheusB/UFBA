72cf319315dba01728a07a687f7a13062f7dc190
Your Name <lastb0402@gmail.com>
2025-03-17 16:17:11 -0300

    TERMINEI A SIMULACAO AMEM

--- New_project/libs/simulation.py
+++ New_project/libs/simulation.py
@@ -15,10 +15,11 @@ from casadi import *
 import numpy as np
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
+import torch
 
 
 class Simulation:
-    def __init__(self, plenum_sys, compressor, x0, z0, u0, nAlphas, alphas, nData, dt):
+    def __init__(self, plenum_sys, compressor, x0, z0, u0, nAlphas, alphas, N_RostS, nData, dt, timestep):
         self.plenum = plenum_sys
         self.compressor = compressor
         self.u0 = u0
@@ -28,6 +29,11 @@ class Simulation:
         self.dt = dt
         self.x0 = x0
         self.z0 = z0
+        self.N_RotS = N_RostS
+        self.timestep = timestep
+        self.RNN_train = []
+        self.x_train = []
+        self.y_train = []
     
     def system_residuals(self, y):
         x = y[:3]
@@ -45,7 +51,7 @@ class Simulation:
     def compute_steady_state(self):
         y0 = np.array(self.x0 + self.z0)
     
-        sol = fsolve(self.system_residuals, y0, args = ())
+        sol = fsolve(self.system_residuals, y0, args=())
     
         x_ss = sol[:3]
         z_ss = sol[3:]
@@ -59,7 +65,6 @@ class Simulation:
         z_sym = SX.sym('z', 11)
         u_sym = SX.sym('u', 5)
         
-        # Avaliação do DAE
         ode_sym, alg_sym = self.plenum.evaluate_dae(None, x_sym, z_sym, u_sym)
         dae = {
             'x': x_sym,
@@ -71,34 +76,51 @@ class Simulation:
         
         integrator_solver = integrator('F', 'idas', dae, {'tf': self.dt})
         
-        # Listas para armazenar os resultados
         time_steps = []
         x_values = []
         z_values = []
-        alpha_values = []
+        alpha_values = []  
+        N_values = []
         
         time = 0
         
         for i in range(self.nAlphas):
             self.u0[3] = self.alphas[i]  # Atualiza a abertura da válvula
-            alpha_value = self.alphas[i] + np.random.normal(0, 0, self.nData)
-            
+            self.u0[2] = self.N_RotS[i]
             for j in range(self.nData):
                 res = integrator_solver(x0=x_ss, z0=z_ss, p=self.u0)
                 
-                # Atualiza x0 e z0 para o próximo passo
                 x_ss = np.array(res["xf"])
                 z_ss = np.array(res["zf"])
                 
-                # Armazena os valores
                 time_steps.append(time)
                 x_values.append(x_ss.copy())
                 z_values.append(z_ss.copy())
                 alpha_values.append(self.u0[3])
+                N_values.append(self.u0[2])
                 
+                self.RNN_train.append(np.concatenate((x_ss.flatten(), z_ss.flatten(), [self.u0[3], self.u0[2]])))
                 time += self.dt
+        
         x_values = np.array(x_values)
         z_values = np.array(z_values)
         alpha_values = np.array(alpha_values)
+        self.RNN_train = np.array(self.RNN_train)
+
+        self.x_train = []
+        self.y_train = []
         
-        return x_values, z_values, time_steps, alpha_values
+        for i in range(len(self.RNN_train) - self.timestep):
+            self.x_train.append(self.RNN_train[i:i + self.timestep, [0, 1, 3, 4, 11, -2, -1]])
+            if i + self.timestep < len(self.RNN_train):
+                self.y_train.append(self.RNN_train[i + self.timestep, :14])
+        
+        self.x_train = torch.tensor(np.array(self.x_train), dtype=torch.float32)
+        self.y_train = torch.tensor(np.array(self.y_train), dtype=torch.float32)
+
+        self.x_min = self.x_train.amin(dim=(0, 1), keepdim=True)
+        self.x_max = self.x_train.amax(dim=(0, 1), keepdim=True)
+        self.y_train = self.y_train.unsqueeze(1)
+
+        return x_values, z_values, time_steps, alpha_values, N_values, self.x_train, self.y_train, self.x_min, self.x_max
+
