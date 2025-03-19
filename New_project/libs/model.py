3223ea6fc4824ffa7f5c9aa8bdeb8415ca5fc81f
Matheus <lastb0402@gmail.com>
2025-03-18 21:36:19 -0300

    teste

--- New_project/libs/model.py
+++ New_project/libs/model.py
@@ -4,40 +4,77 @@ import torch.nn as nn
 import numpy as np
 
 class MyModel(nn.Module):
-    def __init__(self, units, A1, Lc, kv, P1, P_out, C, dt, x_min, x_max):
-        self.A1 = A1
-        self.Lc = Lc
-        self.kv = kv
-        self.P1 = P1
-        self.P_out = P_out
-        self.C = C
-        self.dt = dt
-        self.x_min = x_min
+    def __init__(self, units, x_max, x_min):
+        
         self.x_max = x_max
+        self.x_min = x_min
+        
         super(MyModel, self).__init__()
         
         self.rnn_layer = nn.LSTM(
-            input_size=4,
+            input_size=7,
             hidden_size=units,
             batch_first=True,
             bidirectional=False,
             bias=True,
+            num_layers= 2
         )
         
         self.dense_layers = nn.Sequential(
-            nn.Linear(units, 32),
+            nn.Linear(units, 64),
             nn.Tanh(),
-            nn.Linear(32, 32),
-            nn.Tanh(),
-            nn.Linear(32, 2),
+            nn.Linear(64, 14),
         )
 
     def forward(self, inputs):
-        rnn_input = 2 * (inputs - self.x_min) / (self.x_max - self.x_min) - 1
-        
-        rnn_output, _ = self.rnn_layer(rnn_input)
-        
+        rnn_output, _ = self.rnn_layer(inputs)
         dense_output = self.dense_layers(rnn_output[:, -1, :])
-        
-        desnormalizado = ((dense_output + 1) / 2) * (self.x_max[:, :, :2] - self.x_min[:, :, :2]) + self.x_min[:, :, :2]
-        return desnormalizado
+        return dense_output
+    
+    def train_model(self, model, train_loader, val_loader, lr, epochs, optimizers, patience, factor):
+        optimizer = optimizers(model.parameters(), lr=lr, weight_decay=1e-4)
+        scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor = factor, patience = patience, verbose=True)
+
+        model.train()
+
+        # Listas para armazenar as perdas
+        train_loss_values = []
+        val_loss_values = []
+
+        for epoch in range(epochs):
+            # Treinamento
+            model.train()
+            total_loss = 0
+
+            for inputs, y_true in train_loader:
+                optimizer.zero_grad()
+                y_pred = self(inputs)
+                loss = nn.functional.mse_loss(y_pred, y_true)  # MSE único
+                loss.backward()
+                torch.nn.utils.clip_grad_norm_(self.parameters(), 1.0)  # Gradient clipping
+                optimizer.step()
+
+                total_loss += loss.item()
+            
+            # Atualizar o scheduler
+            scheduler.step(total_loss / len(train_loader))
+
+            train_loss_values.append(total_loss / len(train_loader))
+            
+            model.eval()
+            with torch.no_grad():
+                val_loss = 0
+                for val_inputs, val_y_true in val_loader:
+                    val_y_pred = model(val_inputs)
+                    val_data_loss = nn.functional.mse_loss(val_y_pred, val_y_true)
+                    val_loss += val_data_loss.item()
+
+                val_loss /= len(val_loader)
+                val_loss_values.append(val_loss)
+
+            print(f"Epoch [{epoch + 1}/{epochs}], "
+                f"Train Loss: {train_loss_values[-1]:.6f}, Val Loss: {val_loss_values[-1]:.6f}"
+                f"Learning Rate: {scheduler.get_last_lr()[0]:.6f}")
+
+
+        return train_loss_values, val_loss_values
