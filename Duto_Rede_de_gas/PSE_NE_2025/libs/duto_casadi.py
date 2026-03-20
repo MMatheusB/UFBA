c6b98cafee9a8a1916679b60ebaa65c47dd3443c
MMatheusB <lastb0402@gmail.com>
2026-03-20 14:54:41 -0300

    Add PSE-NE files

---
+++ Duto_Rede_de_gas/PSE_NE_2025/libs/duto_casadi.py
@@ -0,0 +1,396 @@
+from casadi import MX, vertcat, cos, pi, log10, sqrt, inv, arccosh, fabs
+import numpy as np
+from libs.gc_eos_soave import *
+from libs.viscosity import *
+from libs.eos_database import *
+from libs.composicaogas import *
+
+
+class duto_casadi:
+    def __init__(self, gas, visc, Lc, compressor, D, n_points=21):
+        self.visc = visc
+        self.gas = gas
+        self.Lc = Lc
+        self.D = D
+        self.e_D = 1.5 * 1e-6  # m
+        self.k_solo = 0.89  # w / (m*K)
+        self.T_solo = 15 + 273.15  # K
+        self.z_solo = 2  # m
+        self.compressor = compressor
+        i = np.arange(1, n_points + 1)
+        xi = np.cos((2 * i - 1) / (2 * n_points) * np.pi)  # [-1,1]
+        self.l = (self.Lc / (xi[-1] - xi[0])) * (xi - xi[0])  # [0, Lc]
+        self.n_points = n_points
+
+    def fator_friccao(self, Re):
+        """
+        Implementação simbólica do fator de fricção usando CasADi.
+        """
+        eD = self.e_D / self.D
+        expr = -4 * log10(eD / 3.7 - 5.02 / Re * log10(eD / 3.7 - 5.02 / Re * log10(eD / 3.7 + 13 / Re)))
+        return 4 * (expr)**(-2)
+
+    def q_solo(self, Rho, T, U):
+        return (1 / Rho) * (4 * U / self.D) * (self.T_solo - T)
+
+    def coef_cov_fluid(self, kappa, mu, Re, gas):
+        Pr = (gas.Cpt * 1000 / gas.mixture.MM_m * mu) / kappa
+        ft = (1.82 * log10(Re) - 1.64)**(-2)
+        Nu = (ft / 8) * (Re - 1000) * Pr / (1.07 + 12.7 * sqrt(ft / 8) * (Pr**(2/3) - 1))
+        h_t = Nu * kappa / self.D
+        return h_t
+
+    def derivada_centrada(self, x, f, i):
+        """
+        Adaptado para CasADi: f é uma lista de MX e x é numpy.
+        """
+        n = len(x)
+        if i == 0:
+            idx = [0, 1, 2]
+        elif i == n - 1:
+            idx = [n - 3, n - 2, n - 1]
+        else:
+            idx = [i - 1, i, i + 1]
+
+        x_sub = [x[j] for j in idx]
+        f_sub = [f[j] for j in idx]
+        xi = x[i]
+
+        deriv = SX(0)
+        for j in range(3):
+            Lj_deriv = 0
+            for m in range(3):
+                if m != j:
+                    prod = 1
+                    for k in range(3):
+                        if k != j and k != m:
+                            prod *= (xi - x_sub[k]) / (x_sub[j] - x_sub[k])
+                    Lj_deriv += prod / (x_sub[j] - x_sub[m])
+            deriv += f_sub[j] * Lj_deriv
+        return deriv
+
+    def evaluate_alg(self, T_in, V_in, w_in, w_aspas, V_aspas, T_aspas):
+        A = ca.pi * (self.D**2) / 4
+        MM = self.gas.mixture.MM_m  # massa molar em kg/mol
+        v_kg = V_in / MM
+        rho = 1 / v_kg
+        m_dot_final = rho * A * w_in
+        v_kg_aspas = V_aspas / MM
+        rho_aspas = 1 / v_kg_aspas
+        m_dot_aspas = rho_aspas * A * w_aspas
+        ag1 = m_dot_final * (self.gas.h - self.gas.h) + T_in * self.gas.dPdT * V_in* (m_dot_final - m_dot_aspas)
+        gas_in = self.gas.copy_change_conditions(T_in, None, V_in, 'gas')
+        gas_aspas = self.gas.copy_change_conditions(T_aspas, None, V_aspas, 'gas')
+        ag2 = gas_in.P - gas_aspas.P
+        ag3 = m_dot_final - m_dot_aspas
+        
+        return  ag1, ag2, ag3
+
+    def evaluate_dae(self, t, y, z, u):
+        T = [y[3*i + 0] for i in range(self.n_points)]
+        V = [y[3*i + 1] for i in range(self.n_points)]
+        w = [y[3*i + 2] for i in range(self.n_points)]
+
+        Timp = z[0]   # Temperatura na entrada do compressor
+        Vimp = z[1]   
+        Tdif = z[2]   
+        Vdif = z[3]   
+        T2s  = z[4]   
+        V2s  = z[5]   
+        T2   = z[6]   
+        V2   = z[7]   
+        V1   = z[8]   
+        m_dot_inicio = z[9]
+        P_final = z[10]
+
+        rot = u[0] 
+        P1 = u[1]
+        T1 = u[2]
+        Q_final = u[3]
+
+        A = np.pi * (self.D**2) / 4
+
+        MM = self.gas.mixture.MM_m  # massa molar em kg/mol
+        v_kg = V[0] / MM
+        rho = 1 / v_kg
+        m_dot = rho * A * w[0]
+        a1, a2, a3, a4, a5, a6, a7, a8, a9 = self.compressor.character_dae(
+            [Timp, Vimp, Tdif, Vdif, T2s, V2s, T2, V2, V1],
+            [rot, (m_dot)/4, P1, T1]   
+        )
+        a10 = m_dot_inicio - m_dot
+        
+        gas_temp = self.gas.copy_change_conditions(T[-1], None, V[-1], 'gas')
+        
+        a11 = P_final - gas_temp.P
+
+        w_final = Q_final/A
+        dTdt, dVdt, dwdt = [], [], []
+        for i in range(self.n_points):
+            # Atualiza propriedades do gás
+            gas2 = self.gas.copy_change_conditions(T[i], None, V[i], 'gas')
+            v_kg = V[i] / gas2.mixture.MM_m
+            rho = 1 / v_kg
+            gas2.ci_real()
+            Cv = gas2.Cvt / gas2.mixture.MM_m * 1000
+            mu = self.visc.evaluate_viscosity(T[i], gas2.P)
+            Re = rho * w[i] * self.D / mu
+            f = self.fator_friccao(Re)
+            kappa = coef_con_ter(gas2)
+            h_t = self.coef_cov_fluid(kappa, mu, Re, gas2)
+            U = 1 / ((1 / h_t) + (self.D / (2 * self.k_solo)) * arccosh(2 * self.z_solo / self.D))
+            q = self.q_solo(rho, T[i], U)
+            dPdT = gas2.dPdT * 1000
+            dPdV = gas2.dPdV * 1000
+
+            # Derivadas espaciais
+            dT_dx = self.derivada_centrada(self.l, T, i)
+            dV_dx = self.derivada_centrada(self.l, V, i)
+            dw_dx = self.derivada_centrada(self.l, w, i)
+
+            # Matriz A simbólica
+            A = vertcat(
+                horzcat(-w[i], 0, -T[i] * (v_kg * dPdT / Cv)),
+                horzcat(0, -w[i], V[i]),
+                horzcat(-v_kg * dPdT, -v_kg * dPdV, -w[i])
+            )
+
+            dx = vertcat(dT_dx, dV_dx, dw_dx)
+
+            b = vertcat(
+                f * w[i]**2 * fabs(w[i]) / (2 * self.D * Cv) + q / Cv,
+                SX(0),
+                -f * w[i] * fabs(w[i]) / (2 * self.D)
+            )
+
+            result = A @ dx + b
+
+            dTdt.append(result[0])
+            dVdt.append(result[1])
+            dwdt.append(result[2])
+
+            # Condições de fronteira
+            if i == 0:
+                dTdt[i] = (T2 - T[i])
+                dVdt[i] = (V2 - V[i])
+            elif i == self.n_points - 1:
+                dwdt[i] = (w_final - w[i])
+
+        # Concatenar tudo
+        dydt = []
+        for i in range(self.n_points):
+            dydt += [dTdt[i], dVdt[i], dwdt[i]]
+        
+        alg = vertcat(a1, a2, a3, a4, a5, a6, a7, a8, a9, a10, a11)
+
+        return vertcat(*dydt), alg
+    
+    def evaluate_dae_duto2(self, t, y, z, u):
+        T = [y[3*i + 0] for i in range(self.n_points * 2)]
+        V = [y[3*i + 1] for i in range(self.n_points * 2)]
+        w = [y[3*i + 2] for i in range(self.n_points * 2)]
+
+        Timp_1 = z[0]   # Temperatura na entrada do compressor
+        Vimp_1 = z[1]   # Vazão na entrada do compressor
+        Tdif_1 = z[2]   # Temperatura na saída do compressor
+        Vdif_1 = z[3]   # Vazão na saída do compressor
+        T2s_1 = z[4]   
+        V2s_1 = z[5]   
+        T2_1 = z[6]   
+        V2_1 = z[7]   
+        V1_1 = z[8]   
+        T_aspas = z[9]
+        V_aspas = z[10]
+        w_aspas = z[11]
+        Timp_2 = z[12]   # Temperatura na entrada do compressor
+        Vimp_2 = z[13]   # Vazão na entrada do compressor
+        Tdif_2 = z[14]   # Temperatura na saída do compressor
+        Vdif_2 = z[15]   # Vazão na saída do compressor
+        T2s_2 = z[16]   
+        V2s_2 = z[17]   
+        T2_2 = z[18]   
+        V2_2 = z[19]   
+        V1_2 = z[20]
+        
+        rot = u[0] 
+        P1 = u[1]
+        T1 = u[2]
+        P_duto = u[3]
+        rot2 = u[4]
+        Q_final = u[5]
+
+        A = np.pi * (self.D**2) / 4
+
+        MM = self.gas.mixture.MM_m  # massa molar em kg/mol
+        v_kg = V[0] / MM
+        rho = 1 / v_kg
+        m_dot = rho * A * w[0]
+        a3, a4, a5, a6, a7, a8, a9, a10, a11 = self.compressor.character_dae(
+            [Timp_1, Vimp_1, Tdif_1, Vdif_1, T2s_1, V2s_1, T2_1, V2_1, V1_1],
+            [rot, (m_dot)/4, P1, T1]   
+        )
+        a12, a13, a14 = self.evaluate_alg(T[-1], V[-1],w[-1], w_aspas, V_aspas, T_aspas)
+        w_final = Q_final/A
+        dTdt, dVdt, dwdt = [], [], []
+        for i in range(self.n_points):
+            # Atualiza propriedades do gás
+            gas2 = self.gas.copy_change_conditions(T[i], None, V[i], 'gas')
+            v_kg = V[i] / gas2.mixture.MM_m
+            rho = 1 / v_kg
+            gas2.ci_real()
+            Cv = gas2.Cvt / gas2.mixture.MM_m * 1000
+            mu = self.visc.evaluate_viscosity(T[i], gas2.P)
+            Re = rho * w[i] * self.D / mu
+            f = self.fator_friccao(Re)
+            kappa = coef_con_ter(gas2)
+            h_t = self.coef_cov_fluid(kappa, mu, Re, gas2)
+            U = 1 / ((1 / h_t) + (self.D / (2 * self.k_solo)) * arccosh(2 * self.z_solo / self.D))
+            q = self.q_solo(rho, T[i], U)
+            dPdT = gas2.dPdT * 1000
+            dPdV = gas2.dPdV * 1000
+
+            # Derivadas espaciais
+            dT_dx = self.derivada_centrada(self.l, T[:self.n_points+1], i)
+            dV_dx = self.derivada_centrada(self.l, V[:self.n_points+1], i)
+            dw_dx = self.derivada_centrada(self.l, w[:self.n_points+1], i)
+
+            # Matriz A simbólica
+            A = ca.vertcat(
+                ca.horzcat(-w[i], 0, -T[i] * (v_kg * dPdT / Cv)),
+                ca.horzcat(0, -w[i], V[i]),
+                ca.horzcat(-v_kg * dPdT, -v_kg * dPdV, -w[i])
+            )
+
+            dx = ca.vertcat(dT_dx, dV_dx, dw_dx)
+
+            b = ca.vertcat(
+                f * w[i]**2 * fabs(w[i]) / (2 * self.D * Cv) + q / Cv,
+                SX(0),
+                -f * w[i] * fabs(w[i]) / (2 * self.D)
+            )
+
+            result = A @ dx + b
+
+            dTdt.append(result[0])
+            dVdt.append(result[1])
+            dwdt.append(result[2])
+
+            # Condições de fronteira
+            if i == 0:
+                dTdt[i] = (T2_1 - T[i])
+                dVdt[i] = (V2_1 - V[i])
+            elif i == self.n_points - 1:
+                dwdt[i] = (w_final - w[i])
+        
+        v_kg = V[self.n_points] / MM
+        rho = 1 / v_kg
+        m_dot = rho * A * w[self.n_points]
+        gas_temp = self.gas.copy_change_conditions(T_aspas, None, V_aspas, 'gas')
+
+        a15, a16, a17, a18, a19, a20, a21, a22, a23 = self.compressor.character_dae(
+            [Timp_2, Vimp_2, Tdif_2, Vdif_2, T2s_2, V2s_2, T2_2, V2_2, V1_2],
+            [rot2, (m_dot)/4, gas_temp.P, T_aspas]
+        )
+        for i in range(self.n_points):
+            # Atualiza propriedades do gás
+            gas2 = self.gas.copy_change_conditions(T[i], None, V[i], 'gas')
+            v_kg = V[i] / gas2.mixture.MM_m
+            rho = 1 / v_kg
+            gas2.ci_real()
+            Cv = gas2.Cvt / gas2.mixture.MM_m * 1000
+            mu = self.visc.evaluate_viscosity(T[i], gas2.P)
+            Re = rho * w[i] * self.D / mu
+            f = self.fator_friccao(Re)
+            kappa = coef_con_ter(gas2)
+            h_t = self.coef_cov_fluid(kappa, mu, Re, gas2)
+            U = 1 / ((1 / h_t) + (self.D / (2 * self.k_solo)) * arccosh(2 * self.z_solo / self.D))
+            q = self.q_solo(rho, T[i], U)
+            dPdT = gas2.dPdT * 1000
+            dPdV = gas2.dPdV * 1000
+
+            # Derivadas espaciais
+            dT_dx = self.derivada_centrada(self.l, T[self.n_points:], i)
+            dV_dx = self.derivada_centrada(self.l, V[self.n_points:], i)
+            dw_dx = self.derivada_centrada(self.l, w[self.n_points:], i)
+
+            # Matriz A simbólica
+            A = ca.vertcat(
+                ca.horzcat(-w[i], 0, -T[i] * (v_kg * dPdT / Cv)),
+                ca.horzcat(0, -w[i], V[i]),
+                ca.horzcat(-v_kg * dPdT, -v_kg * dPdV, -w[i])
+            )
+
+            dx = ca.vertcat(dT_dx, dV_dx, dw_dx)
+
+            b = ca.vertcat(
+                f * w[i]**2 * fabs(w[i]) / (2 * self.D * Cv) + q / Cv,
+                SX(0),
+                -f * w[i] * fabs(w[i]) / (2 * self.D)
+            )
+
+            result = A @ dx + b
+
+            dTdt.append(result[0])
+            dVdt.append(result[1])
+            dwdt.append(result[2])
+
+            # Condições de fronteira
+            if i == 0:
+                dTdt[i] = (T2_2 - T[i])
+                dVdt[i] = (V2_2 - V[i])
+            elif i == self.n_points - 1:
+                dwdt[i] = (w_final - w[i])
+        # Concatenar tudo
+        dydt = []
+        for i in range(self.n_points):
+            dydt += [dTdt[i], dVdt[i], dwdt[i]]
+        
+        alg = ca.vertcat(a3, a4, a5, a6, a7, a8, a9, a10, a11, a12, a13, a14, a15, a16, a17, a18, a19, a20, a21, a22, a23)
+
+        return ca.vertcat(*dydt), alg
+    
+    def estacionario(self, x, y):
+
+        T, V, w = y[0], y[1], y[2]
+
+        # Copia o gás e atualiza as condições
+        gas2 = self.gas.copy_change_conditions(T, None, V, 'gas')
+        v_kg = V / gas2.mixture.MM_m
+        rho = 1 / v_kg
+        gas2.ci_real()
+
+        Cv = gas2.Cvt / gas2.mixture.MM_m * 1000
+        mu = self.visc.evaluate_viscosity(T, gas2.P)
+        Re = rho * w * self.D / mu
+
+        # Fator de fricção
+        f = self.fator_friccao(Re)
+        kappa = coef_con_ter(gas2)
+        h_t = self.coef_cov_fluid(kappa, mu, Re, gas2)
+        U = 1 / ((1 / h_t) + (self.D / (2 * self.k_solo)) * arccosh(2 * self.z_solo / self.D))
+        q = self.q_solo(rho, T, U)
+
+        dPdT = gas2.dPdT * 1000
+        dPdV = gas2.dPdV * 1000
+
+        # Matriz A e vetor b simbólicos
+        A = vertcat(
+            horzcat(-w, 0, -T * (v_kg * dPdT / Cv)),
+            horzcat(0, -w, V),
+            horzcat(-v_kg * dPdT, -v_kg * dPdV, -w)
+        )
+
+        b = vertcat(
+            f * w**2 * fabs(w) / (2 * self.D * Cv) + q / Cv,
+            0,
+            -f * w * fabs(w) / (2 * self.D)
+        )
+
+        # Sistema linear A * [dTdx, dVdx, dwdx] = -b
+        deriv = -inv(A) @ b
+
+        dTdx = deriv[0]
+        dVdx = deriv[1]
+        dwdx = deriv[2]
+
+        return vertcat(dTdx, dVdx, dwdx)
---
+++ Duto_Rede_de_gas/PSE_NE_2025/libs/simulation_duto.py
@@ -0,0 +1,259 @@
+import numpy as np
+import casadi as ca
+import matplotlib.pyplot as plt
+import torch
+
+
+class SimuladorDuto:
+    def __init__(self, sistema):
+        self.sistema = sistema
+        self.integrador = None
+        self.resultados = {}
+
+    def run(self, y0, z0, u0, N_perturb, N_data, Rot, dt):
+        y = ca.SX.sym("y", 3 * self.sistema.n_points)
+        z = ca.SX.sym("z", 11)  
+        u = ca.SX.sym("u", 4)
+        t = ca.SX.sym("t")
+
+        dydt, alg_eqs = self.sistema.evaluate_dae(t, y, z, u)
+        dae = {"x": y, "z": z, "p": u, "ode": dydt, "alg": alg_eqs}
+
+        self.integrador = ca.integrator(
+            "integrador", "idas", dae,
+            {"tf": dt, "abstol": 1e-8, "reltol": 1e-8, "calc_ic": True}
+        )
+
+        y0 = np.array(y0, dtype=float)
+        z0 = np.array(z0, dtype=float).flatten()
+        u_current = np.array(u0, dtype=float)
+
+        n_points = self.sistema.n_points
+        n_diff = len(y0)
+        n_alg = len(z0)
+
+        y_sol = np.zeros((N_data*N_perturb, n_diff))
+        z_sol = np.zeros((N_data*N_perturb, n_alg))
+
+        y_sol[0, :] = y0
+        z_sol[0, :] = z0
+
+        y_current, z_current = y0, z0
+        rot_array = []
+
+        for i in range(N_perturb):
+            u_current[0] = Rot[i]
+
+            for j in range(N_data):
+
+                sol = self.integrador(
+                    x0=ca.DM(y_current),
+                    z0=ca.DM(z_current),
+                    p=ca.DM(u_current)
+                )
+
+                y_current = np.array(sol["xf"]).flatten()
+                z_current = np.array(sol["zf"]).flatten()
+
+                idx = i * N_data + j 
+
+                y_sol[idx, :] = y_current
+                z_sol[idx, :] = z_current
+                rot_array.append(u_current[0])
+
+        T_sol = np.zeros((N_data*N_perturb, n_points))
+        V_sol = np.zeros((N_data*N_perturb, n_points))
+        w_sol = np.zeros((N_data*N_perturb, n_points))
+
+        for j in range(n_points):
+            T_sol[:, j] = y_sol[:, 3*j + 0]
+            V_sol[:, j] = y_sol[:, 3*j + 1]
+            w_sol[:, j] = y_sol[:, 3*j + 2]
+
+        P_out = np.zeros(N_data*N_perturb)
+        m_dot_sol = np.zeros((N_data*N_perturb, n_points))
+        A = np.pi * (self.sistema.D / 2)**2 
+
+        for i in range(N_data*N_perturb):
+            T_out = T_sol[i, -1]
+            V_out = V_sol[i, -1]
+            v_kg = V_sol[i, :] / self.sistema.gas.mixture.MM_m
+            gas2 = self.sistema.gas.copy_change_conditions(T_out, None, V_out, 'gas')
+            P_out[i] = gas2.P  
+            m_dot_sol[i, :] = (1 / v_kg) * w_sol[i, :] * A
+
+        # ARMAZENAR RESULTADOS + Z[9] E Z[10]
+        self.resultados = {
+            "tempo": np.linspace(0, N_data*N_perturb*dt, N_data*N_perturb),
+            "T_sol": T_sol,
+            "V_sol": V_sol,
+            "w_sol": w_sol,
+            "P_out": P_out,
+            "m_dot": m_dot_sol,
+            "z_sol": z_sol,
+            "rot": rot_array,
+            "z10": z_sol[:, 9],   # penúltima variável algébrica
+            "z11": z_sol[:, 10]   # última variável algébrica
+        }
+
+        return self.resultados
+    
+    def train_dataset(self, time_step=5):
+
+        T_sol = self.resultados["T_sol"]
+        V_sol = self.resultados["V_sol"]
+        w_sol = self.resultados["w_sol"]
+        m_dot = self.resultados["m_dot"]
+
+        n_points = self.sistema.n_points
+        N_total = T_sol.shape[0]
+
+        X_list = []
+        Y_list = []
+
+        for j in range(n_points):
+
+            pos = j  
+
+            for t in range(time_step, N_total - 1):
+
+                x_window = []
+
+                for k in range(time_step):
+
+                    i = t - time_step + k
+
+                    # -------- INLET --------
+                    T_in = T_sol[i, 0]
+                    m_in = m_dot[i, 0]
+
+                    V_in = V_sol[i, 0]
+                    P_in = self.sistema.gas.copy_change_conditions(T_in, None, V_in, 'gas').P
+
+                    # -------- OUTLET --------
+                    T_out = T_sol[i, -1]
+                    m_out = m_dot[i, -1]
+
+                    V_out = V_sol[i, -1]
+                    P_out = self.sistema.gas.copy_change_conditions(T_out, None, V_out, 'gas').P
+
+                    x_window.append([
+                        pos,
+                        T_in, m_in, P_in,
+                        T_out, m_out, P_out
+                    ])
+
+                T_next = T_sol[t+1, j]
+                V_next = V_sol[t+1, j]
+                w_next = w_sol[t+1, j]
+                m_next = m_dot[t+1, j]
+
+                P_next = self.sistema.gas.copy_change_conditions(
+                    T_next, None, V_next, 'gas'
+                ).P
+
+                y = [T_next, V_next, w_next, P_next, m_next]
+
+                X_list.append(x_window)
+                Y_list.append(y)
+
+        # -------- TENSORES --------
+        x_train = torch.tensor(np.array(X_list), dtype=torch.float32)
+        y_train = torch.tensor(np.array(Y_list), dtype=torch.float32)
+
+        # -------- NORMALIZAÇÃO --------
+        x_min = x_train.amin(dim=(0,1), keepdim=True)
+        x_max = x_train.amax(dim=(0,1), keepdim=True)
+
+        y_min = y_train.amin(dim=0, keepdim=True)
+        y_max = y_train.amax(dim=0, keepdim=True)
+
+        return x_train, y_train, x_min, x_max, y_min, y_max
+
+    def plotar(self):
+        if not self.resultados:
+            print("⚠️ Nenhum resultado disponível. Rode a simulação primeiro.")
+            return
+
+        t_h = self.resultados["tempo"] / 3600
+        T_sol = self.resultados["T_sol"]
+        w_sol = self.resultados["w_sol"]
+        V_sol = self.resultados["V_sol"]
+        P_out = self.resultados["P_out"]
+        m_dot_sol = self.resultados["m_dot"]
+
+        z10 = self.resultados["z10"]
+        z11 = self.resultados["z11"]
+
+        n_points = self.sistema.n_points
+
+        # --- Temperatura ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução da Temperatura ao longo do duto")
+        for i in range(n_points):
+            plt.plot(t_h, T_sol[:, i], label=f"Nó {i+1}")
+        plt.xlabel("Tempo / h")
+        plt.ylabel("Temperatura / K")
+        plt.grid(True)
+        plt.legend(fontsize=8)
+        plt.show()
+
+        # --- Velocidade ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução da Velocidade do Gás")
+        for i in range(n_points):
+            plt.plot(t_h, w_sol[:, i], label=f"Nó {i+1}")
+        plt.xlabel("Tempo / h")
+        plt.ylabel("Velocidade / m/s")
+        plt.grid(True)
+        plt.legend(fontsize=8)
+        plt.show()
+
+        # --- Volume específico ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução do Volume Específico")
+        for i in range(n_points):
+            plt.plot(t_h, V_sol[:, i], label=f"Nó {i+1}")
+        plt.xlabel("Tempo / h")
+        plt.ylabel("Volume específico / m³/mol")
+        plt.grid(True)
+        plt.legend(fontsize=8)
+        plt.show()
+
+        # --- Vazão mássica ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução da Vazão Mássica")
+        for i in range(n_points):
+            plt.plot(t_h, m_dot_sol[:, i], label=f"Nó {i+1}")
+        plt.xlabel("Tempo / h")
+        plt.ylabel("Vazão mássica / kg/s")
+        plt.grid(True)
+        plt.legend(fontsize=8)
+        plt.show()
+
+        # # --- Pressão na saída ---
+        # plt.figure(figsize=(9, 6))
+        # plt.plot(t_h, P_out, color='tab:red', linewidth=2)
+        # plt.title("Pressão na Saída do Duto")
+        # plt.xlabel("Tempo / h")
+        # plt.ylabel("Pressão / kPa")
+        # plt.grid(True)
+        # plt.show()
+
+        # --- z[10] ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução da Vazão Mássica no No 1")
+        plt.plot(t_h, z10, linewidth=2)
+        plt.xlabel("Tempo / h")
+        plt.ylabel("z[10]")
+        plt.grid(True)
+        plt.show()
+
+        # --- z[11] ---
+        plt.figure(figsize=(9, 6))
+        plt.title("Evolução da Pressão na Saída do Duto")
+        plt.plot(t_h, z11, linewidth=2, color="purple")
+        plt.xlabel("Tempo / h")
+        plt.ylabel("z[11]")
+        plt.grid(True)
+        plt.show()
